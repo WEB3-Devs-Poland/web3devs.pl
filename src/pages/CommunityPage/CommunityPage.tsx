dc079a80fabafdac8389123e33d5f739bf5aa5ca
Kacper Adler <kacper00adler@gmail.com>
2022-05-29 17:11:42 +0200

    Update community naigation

--- src/pages/CommunityPage/CommunityPage.tsx
+++ src/pages/CommunityPage/CommunityPage.tsx
@@ -1,9 +1,21 @@
+import useIPFSNavigate from 'providers/IPFSRouter/hooks/useIPFSNavigate';
+import { useTranslation } from 'react-i18next';
+import { IoMdArrowBack } from 'react-icons/io';
+
 import * as S from './CommunityPage.styles';
 import { ourCommunityMembers } from './community.config';
 
 const CommunityPage = () => {
+  const { t } = useTranslation();
+  const navigate = useIPFSNavigate();
+
   return (
     <S.Content>
+      <S.TopLineContent>
+        <S.BackButton onClick={() => navigate('/')}>
+          <IoMdArrowBack /> {t('community.backButton')}
+        </S.BackButton>
+      </S.TopLineContent>
       {ourCommunityMembers.map((member) => (
         <S.Card key={member.name}>
           <S.MemberIMG src={member.img} $addBackground={member.addBackground} />
