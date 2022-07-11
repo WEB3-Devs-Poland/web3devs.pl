9b0b725f27f51f9d06e26d79be89ff7aa73537ee
Kacper Adler <kacper00adler@gmail.com>
2022-07-11 17:58:33 +0200

    Back to standard loading

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import { IoMdArrowBack } from 'react-icons/io';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import Loading from 'components/Loading';
 import useLocale from 'translations/hooks';
 import { getArticle, getArticles } from 'utilities/getArticles';
 import { MetaDataType, parseMetaDataFromArticle } from 'utilities/parseMetaDataFromArticle';
@@ -17,24 +16,14 @@ export const BlogPage = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
   const [listOfArticles, setListOfArticles] = useState<MetaDataType[]>([]);
 
   useEffect(() => {
-    try {
-      setIsLoading(true);
-      getArticles()
-        .map((file) => getArticle(file).then((res) => parseMetaDataFromArticle(res)))
-        .forEach((item) =>
-          item.then((article) => setListOfArticles((prevState) => [...prevState, article]))
-        );
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+    getArticles()
+      .map((file) => getArticle(file).then((res) => parseMetaDataFromArticle(res)))
+      .forEach((item) =>
+        item.then((article) => setListOfArticles((prevState) => [...prevState, article]))
+      );
   }, []);
 
   return (
@@ -50,25 +39,19 @@ export const BlogPage = () => {
                 <IoMdArrowBack /> {t('community.backButton')}
               </S.BackButton>
             </S.TopLineContent>
-            {isLoading ? (
-              <Loading />
-            ) : isError ? (
-              <>{t('error')}</>
-            ) : (
-              listOfArticles
-                .sort((article1, article2) => compareDesc(article1.date, article2.date))
-                .map((article) => (
-                  <S.PostLinkContent
-                    onClick={() => navigate(`/blog/${article.path}`)}
-                    key={article.path}
-                  >
-                    <S.PostDate>
-                      {article.date.toLocaleDateString(locale.locale, { dateStyle: 'short' })}
-                    </S.PostDate>
-                    <S.PostLink>{article.title}</S.PostLink>
-                  </S.PostLinkContent>
-                ))
-            )}
+            {listOfArticles
+              .sort((article1, article2) => compareDesc(article1.date, article2.date))
+              .map((article) => (
+                <S.PostLinkContent
+                  onClick={() => navigate(`/blog/${article.path}`)}
+                  key={article.path}
+                >
+                  <S.PostDate>
+                    {article.date.toLocaleDateString(locale.locale, { dateStyle: 'short' })}
+                  </S.PostDate>
+                  <S.PostLink>{article.title}</S.PostLink>
+                </S.PostLinkContent>
+              ))}
           </S.Content>
         }
       />
