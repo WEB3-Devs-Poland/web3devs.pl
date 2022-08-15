a71d82e95db0ca59adda20c98067ad24806ffbf5
Kacper Adler <kacper00adler@gmail.com>
2022-08-15 18:31:54 +0200

    Test what is doing on page

--- src/pages/BlogPage/components/SinglePost/SinglePost.tsx
+++ src/pages/BlogPage/components/SinglePost/SinglePost.tsx
@@ -4,51 +4,54 @@ import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 
 import BackButton from '../../../../components/BackButton/BackButton';
-import { MetaDataType, parseMetaDataFromArticle } from '../../../../utilities/parseMetaDataFromArticle';
-
-import { getArticle, getArticles } from '../../../../utilities/getArticles';
 import Loading from '../../../../components/Loading';
+import { getArticle, getArticles } from '../../../../utilities/getArticles';
+import {
+  MetaDataType,
+  parseMetaDataFromArticle,
+} from '../../../../utilities/parseMetaDataFromArticle';
 import * as S from './SinglePost.styles';
 
 export const SinglePost: React.FC = () => {
-  const { t } = useTranslation();
-  const { postId } = useParams();
+  // const { t } = useTranslation();
+  // const { postId } = useParams();
 
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ isError, setIsError ] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
 
-  const [ post, setPost ] = useState('');
-  const [ , setMetaData ] = useState<MetaDataType>();
+  // const [post, setPost] = useState('');
+  // const [, setMetaData] = useState<MetaDataType>();
   console.log('single post');
 
-  useEffect(() => {
-    setIsLoading(true);
-    getArticles().forEach((file) => {
-      console.log({ file, postId });
-      if (file.includes(postId)) {
-        getArticle(file)
-          .then((data) => {
-            console.log(data);
-            const articleMetaData = parseMetaDataFromArticle(data);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   getArticles().forEach((file) => {
+  //     console.log({ file, postId });
+  //     if (file.includes(postId)) {
+  //       getArticle(file)
+  //         .then((data) => {
+  //           console.log(data);
+  //           const articleMetaData = parseMetaDataFromArticle(data);
 
-            if (articleMetaData.path === postId) {
-              setPost(data[2]);
-              setMetaData(articleMetaData);
-              setIsLoading(false);
-            }
-          })
-          .catch(() => {
-            setIsError(true);
-          });
-      }
+  //           if (articleMetaData.path === postId) {
+  //             setPost(data[2]);
+  //             setMetaData(articleMetaData);
+  //             setIsLoading(false);
+  //           }
+  //         })
+  //         .catch(() => {
+  //           setIsError(true);
+  //         });
+  //     }
 
-      console.log('empty blogpost');
-    });
-  }, [ postId ]);
+  //     console.log('empty blogpost');
+  //   });
+  // }, [ postId ]);
 
   return (
     <S.Content>
-      <div className="my-8">
+      Test what is doing on this page
+      {/* <div className="my-8">
         <BackButton target="/blog" />
       </div>
 
@@ -57,7 +60,7 @@ export const SinglePost: React.FC = () => {
           <S.SinglePost>
             {post}
           </S.SinglePost>
-        )}
+        )} */}
     </S.Content>
   );
 };
