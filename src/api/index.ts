08a6d1fe70ee9a23677bc080bd6b04ae19f251bf
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-14 19:29:59 +0200

    add: api call for deleting address from whitelist

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { DeleteResponseInterface } from 'interfaces/DeleteResponseInterface';
 import { SignatureResponseInterface } from 'interfaces/SignatureResponseInterface';
 
 const API = axios.create({
@@ -6,3 +7,4 @@ const API = axios.create({
 });
 
 export const getSignatureForContract = (hashedMessage: string, address: string) => API.post<SignatureResponseInterface>(`/whitelist/${address}`, { msg: hashedMessage });
+export const deleteAddressFromWhitelist = (address: string) => API.delete<DeleteResponseInterface>(`whitelist/${address}`)
